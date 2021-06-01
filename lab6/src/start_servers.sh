3e10575be679a0e4aaf646b27b204d3477a3e390
targas <ilimar2781@mail.ru>
2021-06-01 09:49:30 +0000

    Script update

--- lab6/src/start_servers.sh
+++ lab6/src/start_servers.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 port=20001
 ip="127.0.0.1"
+filename="/projects/os_lab_2019/lab6/src/Servers.txt"
 echo "Enter number of servers: "
 read num
-echo $num > /projects/os_lab_2019/lab6/src/Servers.txt
+if [ -f "$filename" ]; then
+    read prev_num < "$filename"
+    let num=$num-$prev_num
+    if [ $num -gt 0 ]; then
+        let res=$num+$prev_num
+        sed -i "1s/.*/$res/" "$filename"
+    fi
+else
+    echo "$num" > "$filename"
+fi
 while [[ $num -gt 0 ]]
 do
 if !(lsof -Pi :$port -sTCP:LISTEN -t >/dev/null) ; then
     ./server --port $port --tnum 3 &
-    echo "$ip : $port" >> /projects/os_lab_2019/lab6/src/Servers.txt
+    echo "$ip : $port" >> "$filename"
     let num=$num-1
 fi
 let port+=1
